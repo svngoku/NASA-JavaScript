305ae90d176e4573e2cfa79f34d211305d7d269c
svngoku <32180057+svngoku@users.noreply.github.com>
2018-11-22 07:14:56 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const loadash = require('lodash')
+const nasa = 'NASA';
+
+// Multiplication des valeurs par 3 avec la methode map de lodash
+lodash.map([1,2,3], (n)) => { return n * 3});
+// 3 , 6 , 9
+console.log("We love " + nasa)
